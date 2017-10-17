d421ddd0b78850783f26c93c6cb894587056c8a5
yiuuucen <1784373887@qq.com>
2017-10-17 17:29:36 +0800

    work

--- 2017-10-17/美团微信小程序/app.js
+++ 2017-10-17/美团微信小程序/app.js
@@ -3,14 +3,14 @@ var app = getApp();
 App({
   onLaunch: function() {
     wx.getLocation({
-      type: 'gcj02',
+      //type: 'gcj02',
       success: function (res) {
         var latitude = res.latitude
         var longitude = res.longitude
         wx.request({
           url: 'http://api.map.baidu.com/geocoder/v2/?ak=btsVVWf0TM1zUBEbzFz6QqWF&coordtype=gcj02ll&location=' + latitude + ',' + longitude + '&output=json&pois=0',
           method: "get",
-          success: function (res) {     
+          success: function (res) {
             console.log(res.data.result.formatted_address)
             wx.setStorageSync('location', res.data.result.formatted_address.substr(res.data.result.formatted_address.indexOf('市') + 1, 10))
           }
